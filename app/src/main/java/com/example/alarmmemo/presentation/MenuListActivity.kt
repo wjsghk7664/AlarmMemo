b24731085caa98d6d7d7ce2d4176a082172a9b35
Jisung15 <gimseongjin302@gmail.com>
2024-08-23 12:45:36 +0900

    feat: 리스트 화면 만들고 spinner 추가

---
+++ app/src/main/java/com/example/alarmmemo/presentation/MenuListActivity.kt
@@ -0,0 +1,45 @@
+package com.example.alarmmemo.presentation
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.alarmmemo.R
+import com.example.alarmmemo.databinding.ActivityMenuListBinding
+
+class MenuListActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMenuListBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val spinner : Spinner = binding.MemoListSpListShuffled
+        spinner.adapter = ArrayAdapter.createFromResource(this, R.array.itemList, android.R.layout.simple_spinner_item)
+
+        spinner.onItemSelectedListener = object  : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+            }
+        }
+    }
+}
