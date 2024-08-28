2018f3f1445d26c218b9bba48001921c30dc508d
hamdon12 <chicken010208@gmail.com>
2024-08-28 14:33:51 +0900

    chore: 앱 아이콘 변경

--- app/src/main/java/com/example/alarmmemo/presentation/LoginActivity.kt
+++ app/src/main/java/com/example/alarmmemo/presentation/LoginActivity.kt
@@ -1,24 +1,22 @@
 package com.example.alarmmemo.presentation
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.alarmmemo.R
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+        //startActivity(Intent(this, OssLicensesMenuActivity::class.java))
     }
 }
