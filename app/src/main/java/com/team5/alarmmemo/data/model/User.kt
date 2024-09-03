b26891d0fbb5b3ee16f0e855cf8617df12f5e873
wjsghk7664 <wjsghk7664@naver.com>
2024-09-03 14:16:11 +0900

    feat. 로그인 및 회원가입 레포지토리 완성

---
+++ app/src/main/java/com/team5/alarmmemo/data/model/User.kt
@@ -0,0 +1,11 @@
+package com.team5.alarmmemo.data.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(
+    val emailOrToekn:String ="",
+    val password:String = "",
+    val name:String = ""
+):Parcelable
---
+++ app/src/main/java/com/team5/alarmmemo/data/source/remote/FirebaseModule.kt
@@ -0,0 +1,29 @@
+package com.team5.alarmmemo.data.source.remote
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Provides
+    @Singleton
+    fun provideFirestore(): FirebaseFirestore {
+        return Firebase.firestore
+    }
+
+    @Provides
+    @Singleton
+    fun provideFireStorage(): FirebaseStorage {
+        return Firebase.storage
+    }
+}
