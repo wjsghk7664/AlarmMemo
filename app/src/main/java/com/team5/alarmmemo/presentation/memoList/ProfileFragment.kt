1964a16adb5ed3b13ed9c9247677291216fd9131
Jisung15 <gimseongjin302@gmail.com>
2024-09-05 17:31:54 +0900

    chore : 이름 변경 다이얼로그 추가 및 폰트 수정

--- app/src/main/java/com/team5/alarmmemo/presentation/memoList/ProfileFragment.kt
+++ app/src/main/java/com/team5/alarmmemo/presentation/memoList/ProfileFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.team5.alarmmemo.databinding.ChangeNameDialogBinding
 import com.team5.alarmmemo.databinding.FragmentProfileBinding
 import com.team5.alarmmemo.presentation.memoLogin.LoginActivity
 
@@ -37,7 +39,37 @@ class ProfileFragment : Fragment() {
             profileDeleteButton.setOnClickListener {
                 Toast.makeText(requireContext(), "회원 탈퇴 버튼입니다.", Toast.LENGTH_SHORT).show()
             }
+
+            profileIvNameChangeButton.setOnClickListener {
+                changeName()
+            }
+
+            profileIvPasswordChangeButton.setOnClickListener {
+                Toast.makeText(requireContext(), "비밀번호가 변경되었습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
         }
+    }
+
+    private fun changeName() {
+        val dialogBinding = ChangeNameDialogBinding.inflate(LayoutInflater.from(requireContext()))
+
+        val builder = AlertDialog.Builder(requireContext()).apply {
+            setTitle("이름 변경")
+            setView(dialogBinding.root)
+
+            setPositiveButton("변경") { dialog, _ ->
+                val newName = dialogBinding.dialogInput.text.toString()
+                binding.profileTvNameText.text = newName
+                dialog.dismiss()
+            }
+
+            setNegativeButton("취소") { dialog, _ ->
+                dialog.dismiss()
+            }
+        }
+
+        builder.show()
     }
 
     override fun onDestroyView() {
